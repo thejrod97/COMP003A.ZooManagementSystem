09a47a21db584b749e86abb7a127e3267ec5f606
Justin Rodriguez <jjrod297@gmail.com>
2025-02-21 17:03:54 -0800

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.ZooManagementSystem
+{
+    internal abstract class Animal
+    {
+        // Fields
+        private string _name;
+        private string _species;
+
+        // Properties
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Name can't be null or white space.");
+                _name = value;
+            }
+        }
+
+        // Abstract method with overriding so we can modify the iteration of code within.
+        public abstract void MakeSound(); 
+    }
+}
